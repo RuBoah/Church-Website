55f4bb383249903fe0c401f803d1f071b76d9273
Nathan Sodja <sodjanathan@gmail.com>
2024-05-08 20:01:00 +0000

    added search to attendance page

--- app/dashboard/attendance/page.tsx
+++ app/dashboard/attendance/page.tsx
@@ -1,9 +1,17 @@
-import React from 'react'
+'use client';
+import React from 'react';
+import Search from '@/app/ui/search';
+import Table from '@/app/ui/invoices/table';
 
 function Attendance() {
   return (
-	<div>Attendance</div>
-  )
+    <div>
+      <h1 className="my-5">Attendance</h1>
+      <Search placeholder="Search members..." />
+
+      <table />
+    </div>
+  );
 }
 
-export default Attendance
+export default Attendance;
