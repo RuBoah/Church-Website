e327c17c3fb6406847c8a1d8f2874f8c6acb1b4f
Nathan Sodja <sodjanathan@gmail.com>
2024-05-16 19:47:47 +0000

    setup basic table

--- app/dashboard/attendance/page.tsx
+++ app/dashboard/attendance/page.tsx
@@ -1,17 +1,41 @@
 'use client';
 import React from 'react';
 import Search from '@/app/ui/search';
-import Table from '@/app/ui/invoices/table';
-
+import Datatable from '@/app/ui/datatable';
 function Attendance() {
+  const invoices = [
+    {
+      id: 1,
+      name: 'John Doe',
+      email: 'john@example.com',
+      amount: 100.0,
+      date: '2024-05-16',
+      status: 'Paid',
+    },
+    {
+      id: 2,
+      name: 'Jane Smith',
+      email: 'jane@example.com',
+      amount: 150.0,
+      date: '2024-05-15',
+      status: 'Pending',
+    },
+    // Add more invoices as needed
+  ];
   return (
     <div>
-      <h1 className="my-5">Attendance</h1>
-      <Search placeholder="Search members..." />
-
-      <table />
+      <h1 className="my-5">Member Records</h1>
+      <Search placeholder="Search a member to log attendance..." />
+      <Datatable invoices={invoices} />
+      <h1 className="my-5">Attendance Records</h1>
+      <Datatable invoices={invoices} />
     </div>
   );
 }
 
 export default Attendance;
+
+// install axios
+// TODO what data to show for attendace records
+// TODO name, date, member_id, check_in, check_out, entered_by, service_id,service_name
+// TODO get necessary fields to display from members
