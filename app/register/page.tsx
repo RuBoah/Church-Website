73fc926500f95c1a43cd618ba06d54d89ca27262
nateos <sodjanathan@gmail.com>
2024-05-23 23:27:06 +0000

    register ui basic

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -6,20 +6,50 @@ import Image from 'next/image';
 import {
   AtSymbolIcon,
   KeyIcon,
-  ExclamationCircleIcon,
+  PencilIcon,
 } from '@heroicons/react/24/outline';
 import { ArrowRightIcon } from '@heroicons/react/20/solid';
-import { Button } from './button';
-
+import { Button } from '../ui/button';
 export default function Register() {
   return (
     <main className="flex min-h-screen flex-col p-6">
       <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
         {/* <AcmeLogo /> */}
+        <p className="text-white"> churchday⛪.</p>
       </div>
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
-          <div className={styles.shape} />
+          <div className="">
+            Enter details to register account, if you already have an account,
+            you can{' '}
+            <Link href="/login" className="underline hover:bg-cyan-400">
+              login
+            </Link>
+          </div>
+          <div className="relative">
+            <input
+              className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
+              id="name"
+              type="text"
+              name="name"
+              placeholder="Enter name..."
+              required
+              minLength={6}
+            />
+            <PencilIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
+          </div>
+          <div className="relative">
+            <input
+              className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
+              id="email"
+              type="text"
+              name="email"
+              placeholder="Enter email address"
+              required
+              minLength={6}
+            />
+            <AtSymbolIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
+          </div>
           <div className="relative">
             <input
               className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
@@ -32,11 +62,8 @@ export default function Register() {
             />
             <KeyIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
           </div>
-          <div className="relative">
-
-          </div>
-          
-          <AtSymbolIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
+          <div className="relative"></div>
+          <Button type={'submit'}>Create account</Button>
         </div>
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
           {/* Add Hero Images Here */}
